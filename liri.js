f67cb6e6d659af4e6384cfbc05b72593dfa7998a
Deb Totten <deborahrtotten@gmail.com>
2018-07-13 21:32:15 -0400

    updating after npm install

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var client = new Twitter(keys.twitter);
+var request = require("request");
+var Twitter = require('twitter');
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
