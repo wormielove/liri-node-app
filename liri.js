b0a13fb123b170f2cfa41e042c7a4cf650557388
Deb Totten <deborahrtotten@gmail.com>
2018-07-14 13:16:51 -0400

    submitting

--- liri.js
+++ liri.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
+var request = require("request");
+var command = process.argv[2]
+var Twitter = require('twitter');
 var keys = require("./keys.js");
 var client = new Twitter(keys.twitter);
-var request = require("request");
-var Twitter = require('twitter');
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+var params = { screen_name: 'ernielove7' };
+
+
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
+  if (error) throw error;
+  console.log(tweets);
 });
